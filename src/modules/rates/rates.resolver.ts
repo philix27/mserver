ef016a99a739c9e09e1271b0b1dbc24fc241ff7d
Philix27 <philixbob@gmail.com>
2025-04-27 21:07:23 +0100

    remove guard from rates

--- src/modules/rates/rates.resolver.ts
+++ src/modules/rates/rates.resolver.ts
@@ -1,18 +1,15 @@
-import { Context, Query, Resolver } from "@nestjs/graphql";
-import {
-    ExchangeRate_Response,
-} from "./rates.dto";
-import { UseGuards } from "@nestjs/common";
-import { GqlAuthGuard } from "../common/guards";
-import { ExchangeRateService } from "./rates.service";
-
+import { Context, Query, Resolver } from '@nestjs/graphql';
+import { ExchangeRate_Response } from './rates.dto';
+// import { UseGuards } from '@nestjs/common';
+// import { GqlAuthGuard } from '../common/guards';
+import { ExchangeRateService } from './rates.service';
 
 @Resolver((of: any) => ExchangeRate_Response)
 export class ExchangeRateResolver {
     constructor(private readonly service: ExchangeRateService) {}
 
     @Query((returns) => ExchangeRate_Response)
-    @UseGuards(GqlAuthGuard)
+    // @UseGuards(GqlAuthGuard)
     async fxRate_GetAll(
         @Context() context: { req: { userId: number } },
     ): Promise<ExchangeRate_Response> {
